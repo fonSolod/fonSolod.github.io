97f6a6c15407521b4fa9fefe4f5522813b1348ac
fonSolod <4taksista.i@gmail.com>
2026-08-12 12:37:50 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-// Firebase-конфиг и инициализация. Единственное место, где знаем про Firebase.
+// Firebase-конфиг, база данных и авторизация
 export const firebaseConfig={
 apiKey:"AIzaSyB_GNnhKBop_koWOLjBjyVmfVoWJ_tvF40",
 authDomain:"game-95c86.firebaseapp.com",
@@ -10,11 +10,16 @@ appId:"1:485164734552:web:95a1258519498719d4122d",
 measurementId:"G-F9PW364YX8"
 };
 export const configured=!String(firebaseConfig.apiKey).includes('ВСТАВЬТЕ');
-export let db=null;
+export let db=null,auth=null;
 export let ref=null,update=null,set=null,get=null,onValue=null,onDisconnect=null;
+export let onAuthStateChanged=null,createUserWithEmailAndPassword=null,signInWithEmailAndPassword=null,signOut=null,sendPasswordResetEmail=null;
 if(configured){
 const {initializeApp}=await import("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
 const FDB=await import("https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js");
-ref=FDB.ref;update=FDB.update;set=FDB.set;get=FDB.get;onValue=FDB.onValue;onDisconnect=FDB.onDisconnect;
-db=FDB.getDatabase(initializeApp(firebaseConfig));
+const FA=await import("https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js");
+const app=initializeApp(firebaseConfig);
+({ref,update,set,get,onValue,onDisconnect}=FDB);
+db=FDB.getDatabase(app);
+({onAuthStateChanged,createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,sendPasswordResetEmail}=FA);
+auth=FA.getAuth(app);
 }
