5f0fcaf8bbaf7e62f64ab54e73e1f79490818280
fonSolod <4taksista.i@gmail.com>
2026-08-12 13:31:11 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,9 @@
-// Firebase-конфиг, база данных и авторизация
+// Firebase-конфиг, база данных и авторизация.
+// Инициализация идемпотентна: при повторном вызове модуль использует существующее приложение.
+import {initializeApp, getApps, getApp} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import * as FDB from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import * as FA from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 export const firebaseConfig={
 apiKey:"AIzaSyB_GNnhKBop_koWOLjBjyVmfVoWJ_tvF40",
 authDomain:"game-95c86.firebaseapp.com",
@@ -9,15 +14,14 @@ messagingSenderId:"485164734552",
 appId:"1:485164734552:web:95a1258519498719d4122d",
 measurementId:"G-F9PW364YX8"
 };
+
 export const configured=!String(firebaseConfig.apiKey).includes('ВСТАВЬТЕ');
 export let db=null,auth=null;
 export let ref=null,update=null,set=null,get=null,onValue=null,onDisconnect=null;
 export let onAuthStateChanged=null,createUserWithEmailAndPassword=null,signInWithEmailAndPassword=null,signOut=null,sendPasswordResetEmail=null;
+
 if(configured){
-const {initializeApp}=await import("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
-const FDB=await import("https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js");
-const FA=await import("https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js");
-const app=initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 ({ref,update,set,get,onValue,onDisconnect}=FDB);
 db=FDB.getDatabase(app);
 ({onAuthStateChanged,createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,sendPasswordResetEmail}=FA);
